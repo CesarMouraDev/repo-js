8ecd7b61f6cab8c24f8883e468a844969d1194a0
CesarMouraDev <cesarmouradeveloper@gmail.com>
2024-07-01 11:42:31 -0300

    calculadora 2

--- exercicio-01.js
+++ exercicio-01.js
@@ -1,8 +1,18 @@
-*// Caculador de IMC
 
-var peso = prompt("qual seu peso?")
-var altura = prompt("qual sua altura?")
-var imc = (peso/(altura*altura))/100
-console.log(imc)
 
-if (imc => 18.5)
+var peso = perseFloat("qual seu peso?");
+var altura = perseFloat("qual sua altura?");
+var imc = (peso/(altura*altura))/100;
+console.log(imc);
+
+if (imc >= 18.5 && imc <=24.9) {
+    alert("O IMC indica baixo normal" + imc.toFixed(2));
+}
+    else if (imc >= 25 && imc <= 29.9 ) 
+        alert{"O IMC indica peso peso normal"};
+    else if (imc >= 30 && imc <= 34.9 ) 
+        alert{"O IMC indica peso peso normal"};
+    else if (imc >= 35 && imc <= 39.9 ) 
+        alert{"O IMC indica peso peso normal"};
+    else if (imc >= 40 ) 
+        alert{"O IMC indica peso peso normal"};''
